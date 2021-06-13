36523346663fb0b78c6770865fba4409deb3368e
Brandon-MK <moshkalinaki@gmail.com>
2021-06-13 11:53:28 +0300

    ADD .

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+function ImageSlider() {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" alt="" />
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImageSlider;
+
+const Carousel = styled(Slider)``;
+const Wrap = styled.div``;
