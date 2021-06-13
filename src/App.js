6ded917cd21459cd15d1d3f33dc6699cebcc4db7
Brandon-MK <moshkalinaki@gmail.com>
2021-06-13 15:06:22 +0300

    React-router-dom & details page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
