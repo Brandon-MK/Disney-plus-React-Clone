15f8e722758389c55874fc35e0e93b4e07ccb96f
Brandon-MK <moshkalinaki@gmail.com>
2021-06-13 13:36:19 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Details() {
+  return (
+    <div>
+      <div>Details</div>
+    </div>
+  );
+}
+
+export default Details;
